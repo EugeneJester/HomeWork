a91b208ab056df7ec80eb24c22c39f69037cf257
Veronika <EugeneJester@mail.ru>
2023-02-03 13:10:40 +1200

    Task 4

---
+++ Task_4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+Console.Write("Введите первое число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int B = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int C = Convert.ToInt32(Console.ReadLine());
+
+int max = A;
+if(B>max) max=B;
+if(C>max) max=C;
+Console.Write("Введённое максимальное число: max=");
+Console.WriteLine(max);
+
+Console.WriteLine($"{A}{B}{C} -> {max}");
